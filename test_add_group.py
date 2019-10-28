52a371246e7ef1f518920aa6dd05123a39b297d6
P.S. Potter <ipayac@gmail.com>
2019-10-28 11:39:49 +0300

    New project has been started. Fasten your seat belts and enjoy some voodoo ^_^

--- test_add_group.py
+++ test_add_group.py
@@ -7,6 +7,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
+
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Chrome()
@@ -39,17 +40,22 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_link_text("Logout").click()
     
     def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
         return True
     
     def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
         return True
 
     def tearDown(self):
         self.wd.quit()
+
 
 if __name__ == "__main__":
     unittest.main()
