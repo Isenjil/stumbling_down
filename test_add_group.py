dee42abf0843dfd94445a774184c21aa458b08fe
P.S. Potter <ipayac@gmail.com>
2019-10-30 17:13:51 +0300

    New project has been started. Fasten your seat belts and enjoy some voodoo ^_^

--- test_add_group.py
+++ test_add_group.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.support.ui import Select
+# from selenium.common.exceptions import NoSuchElementException
+# from selenium.common.exceptions import NoAlertPresentException
 from group import Group
 import unittest
 
@@ -15,31 +15,31 @@ class TestAddGroup(unittest.TestCase):
         self.wd.implicitly_wait(30)
 
     def test_add_group(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.add_new_group(wd, Group(name="Nothing", header="Something from Budha", footer="No environment at all"))
-        self.submit_form(wd)
-        self.logout(wd)
+        self.login(username="admin", password="secret")
+        self.add_new_group(Group(name="Nothing", header="Something from Budha", footer="No environment at all"))
+        self.logout()
 
     def test_add_empty_group(self):
+        self.login(username="admin", password="secret")
+        self.add_new_group(Group(name="", header="", footer=""))
+        self.logout()
+
+    def logout(self):
         wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.add_new_group(wd, Group(name="", header="", footer=""))
-        self.submit_form(wd)
-        self.logout(wd)
+        wd.find_element_by_link_text("Logout").click()
 
-    def logout(self, wd):
-       wd.find_element_by_link_text("Logout").click()
-
-    def return_to_groups(self, wd):
+    def return_to_groups(self):
+        wd = self.wd
         wd.find_element_by_link_text("group page").click()
-        self.return_to_groups(wd)
+        self.return_to_groups()
 
-    def submit_form(self, wd):
+    def submit_form(self):
+        wd = self.wd
         wd.find_element_by_name("submit").click()
 
-    def add_new_group(self, wd, group):
-        self.open_groups_page(wd)
+    def add_new_group(self, group):
+        wd = self.wd
+        self.open_groups_page()
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
@@ -51,12 +51,15 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.submit_form()
 
-    def open_groups_page(self, wd):
+    def open_groups_page(self):
+        wd = self.wd
         wd.find_element_by_link_text("add new").click()
 
-    def login(self, wd, username, password):
-        self.open_home_page(wd)
+    def login(self, username, password):
+        wd = self.wd
+        self.open_home_page()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -64,22 +67,23 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def open_home_page(self, wd):
+    def open_home_page(self):
+        wd = self.wd
         wd.get("http://localhost/addressbook/")
 
-    def is_element_present(self, how, what):
-        try:
-            self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-    
-    def is_alert_present(self):
-        try:
-            self.wd.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
+    # def is_element_present(self, how, what):
+    #     try:
+    #         self.wd.find_element(by=how, value=what)
+    #     except NoSuchElementException as e:
+    #         return False
+    #     return True
+    #
+    # def is_alert_present(self):
+    #     try:
+    #         self.wd.switch_to_alert()
+    #     except NoAlertPresentException as e:
+    #         return False
+    #     return True
 
     def tearDown(self):
         self.wd.quit()
