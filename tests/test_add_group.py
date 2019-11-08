7006c5ac52b49eb91aecad0026218ce220fefba9
P.S. Potter <ipayac@gmail.com>
2019-11-08 15:04:02 +0300

    Moved to packages

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from app import App
-from group import Group
+from fixture.app import App
+from model.group import Group
 import pytest
 
 
@@ -11,13 +11,15 @@ def appl(request):
     return fixture
 
 
+def test_add_empty_group(appl):
+    appl.login(username="admin", password="secret")
+    appl.add_new_group(Group(name="", header="", footer=""))
+    appl.logout()
+
+
 def test_add_group(appl):
     appl.login(username="admin", password="secret")
     appl.add_new_group(Group(name="Nothing", header="Something from Budha", footer="No environment at all"))
     appl.logout()
 
 
-def test_add_empty_group(appl):
-    appl.login(username="admin", password="secret")
-    appl.add_new_group(Group(name="", header="", footer=""))
-    appl.logout()
